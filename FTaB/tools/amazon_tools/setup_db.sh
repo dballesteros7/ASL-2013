ff6520b7a28495801141ada0161f3e6c43b532cd
Diego Ballesteros Villamizar <diegob@student.ethz.ch>
2013-11-10 11:20:01 +0100

    Improved script

--- FTaB/tools/amazon_tools/setup_db.sh
+++ FTaB/tools/amazon_tools/setup_db.sh
@@ -9,9 +9,13 @@ mkdir logs
 # Init and start the database server
 /usr/pgsql-9.3/bin/pg_ctl -D postgresql_data/ initdb
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" postgresql_data/postgresql.conf
+sed -i "s/host    all             all             127.0.0.1/32            trust/host    all             all             172.31.0.0/16            trust/g"
 /usr/pgsql-9.3/bin/pg_ctl -D postgresql_data/ -l logs/database.log start
 
 # Create the main database and set appropriate permissions
 # Requires the create_db.sql file
 sleep 5
 psql -d postgres -f create_db.sql
+
+# Setup the IP tables entries
+sudo iptables -I INPUT -i eth0 -p tcp -m tcp --dport 5432 -j ACCEPT
