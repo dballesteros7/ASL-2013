131574129cc3326b78cb5c8125f900603242b5d9
Diego Ballesteros Villamizar <diegob@student.ethz.ch>
2013-11-14 16:48:05 +0100

    Milestone 1 preparation

--- FTaB/delete_query.sql
+++
@@ -1,8 +0,0 @@
-EXPLAIN ANALYZE WITH temp AS (DELETE FROM msg_queue_assoc 
-                WHERE message_id = 576413 AND queue_id = (SELECT id FROM queue WHERE name = 'NotOriginallyNamedQueue1')
-                RETURNING queue_id, message_id) 
-DELETE FROM message USING temp 
-WHERE message.id = temp.message_id AND NOT EXISTS (SELECT 1 FROM msg_queue_assoc msa
-                                                   WHERE msa.message_id = temp.message_id AND 
-                                                   msa.queue_id <> temp.queue_id)
-
