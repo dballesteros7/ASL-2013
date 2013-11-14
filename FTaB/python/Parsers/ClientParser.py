c69a5a59067a36d928b8b10cbe0ba818e447cf4c
Diego Ballesteros Villamizar <diegob@student.ethz.ch>
2013-11-14 16:27:02 +0100

    Tools for report

--- FTaB/python/Parsers/ClientParser.py
+++ FTaB/python/Parsers/ClientParser.py
@@ -13,6 +13,68 @@ from numpy.lib.scimath import sqrt
 from scipy import stats
 
 import matplotlib.pyplot as plt
+from numpy.lib.polynomial import polyfit
+
+
+def plotThroughput(logFile, windowSize = 10000, marker = 's', iteration = 50):
+    fileHandle = open(logFile, 'r')
+    
+
+    wholeFile = fileHandle.read();
+    wholeFile = wholeFile.split("\n")[:-1]
+    wholeFileSorted = sorted(wholeFile, key = lambda x : float(x.split()[2]));
+    
+    globalMin = float(wholeFileSorted[0].split()[2])
+    currentStart = globalMin
+    currentEnd = currentStart + windowSize
+    data = {(currentStart, currentEnd) : 0}
+    for line in wholeFileSorted:
+        min_val = float(line.split()[2])
+        if min_val > currentEnd:
+            currentStart = currentEnd
+            currentEnd = currentStart + windowSize
+            data[(currentStart, currentEnd)] = 0
+        data[(currentStart, currentEnd)] += 1
+
+    x = []
+    y = []
+    for key in sorted(data.keys()):
+        x.append(((key[1] + key[0])/2.0 - globalMin)/1000.0)
+        y.append(data[key]*1000/(key[1] - key[0]))
+    p1, = plt.plot(x,y, marker = marker)
+    ax = plt.gca()
+    #ax.set_ylim([-10, 300])
+    ax.set_xlabel('Experiment time (s)')
+    ax.set_ylabel('Messages read per second')
+    ax.set_title('Throughput of read operations')
+
+    total = []
+    for key in data:
+        if((key[0] - globalMin)/1000. > 1500):
+            continue
+        if((key[0] - globalMin)/1000. < 300):
+            continue
+        total.append(data[key]*1000/(key[1] - key[0]))
+
+    n, min_max, mean, var, _, _ = stats.describe(total)
+    t_test = stats.t.interval(0.95, n - 1, loc=mean, scale=sqrt(var) / sqrt(n))
+    chisquare = stats.chi2.interval(0.95, n - 1)
+    #print "Data samples: %d" % n
+    #print "Average response time: %s ms" % mean
+    #print "Sample standard deviation: %s ms" % sqrt(var)
+   # print "95%% confidence interval for the mean (t-student test): (%s, %s) ms" % stats.t.interval(0.95, n - 1,loc = mean, scale = sqrt(var)/sqrt(n))
+    #chisquare = stats.chi2.interval(0.95, n - 1)
+    #print "95%% confidence interval for the stdev (chi-square test): (%s, %s) ms" % (sqrt((n - 1)*var/chisquare[1]), sqrt((n - 1)*var/chisquare[0]))
+    #m,b =  polyfit(x, y, 1)
+    #print "slope: %s, intercept: %s" % (m, b)
+    #print "$%d$ & $%.1f$ & $%.1f$ & $%.1f-%.1f$ & $%.1f-%.1f$" % (iteration, mean, sqrt(var), stats.t.interval(0.95, n - 1, loc=mean, scale=sqrt(var) / sqrt(n))[0], 
+    #                                                              stats.t.interval(0.95, n - 1, loc=mean, scale=sqrt(var) / sqrt(n))[1],
+    #                                                              sqrt((n - 1)*var/chisquare[1]), 
+    #                                                               sqrt((n - 1)*var/chisquare[0]))
+    print "$%d$ & $%d$ & $%d$ & $%.1f$ & $%.1f$ & $%.1f-%.1f$ & $%.1f-%.1f$ \\\\" % (iteration[0], iteration[1], 
+                                                                                       iteration[2], mean, sqrt(var), t_test[0], t_test[1], sqrt((n - 1)*var/chisquare[1]), sqrt((n - 1)*var/chisquare[0]))
+    fileHandle.close();
+    return p1
 
 def plotAverageResponseTime(logFile, windowSize = 10000, marker = 's', iteration = 50):
     fileHandle = open(logFile, 'r')
@@ -47,16 +109,16 @@ def plotAverageResponseTime(logFile, windowSize = 10000, marker = 's', iteration
     p1, = plt.plot(x,y, marker = marker)
     ax = plt.gca()
     ax.errorbar(x, y, yerr=numpy.vstack([error_low, error_high]))#, marker = marker)
-    ax.set_ylim([-10, 300])
+    #ax.set_ylim([-10, 300])
     ax.set_xlabel('Experiment time (s)')
     ax.set_ylabel('Average response time (ms)')
-    ax.set_title('Average response time to send a message \n (30 seconds window)')
+    ax.set_title('Average database response time to find a queue with messages (if none is found) \n (60 seconds window)')
 
     total = []
     for key in data:
-        if((key[0] - globalMin)/1000. > 500):
+        if((key[0] - globalMin)/1000. > 1500):
             continue
-        if((key[0] - globalMin)/1000. < 100):
+        if((key[0] - globalMin)/1000. < 300):
             continue
         for val in data[key]:
             total.append(val)
@@ -66,31 +128,80 @@ def plotAverageResponseTime(logFile, windowSize = 10000, marker = 's', iteration
     #print "Average response time: %s ms" % mean
     #print "Sample standard deviation: %s ms" % sqrt(var)
     #print "95%% confidence interval for the mean (t-student test): (%s, %s) ms" % stats.t.interval(0.95, n - 1,loc = mean, scale = sqrt(var)/sqrt(n))
+    t_test = stats.t.interval(0.95, n - 1, loc=mean, scale=sqrt(var) / sqrt(n))
     chisquare = stats.chi2.interval(0.95, n - 1)
     #print "95%% confidence interval for the stdev (chi-square test): (%s, %s) ms" % (sqrt((n - 1)*var/chisquare[1]), sqrt((n - 1)*var/chisquare[0]))
-    print "$%d$ & $%.1f$ & $%.1f$ & $%.1f-%.1f$ & $%.1f-%.1f$" % (iteration, mean, sqrt(var), stats.t.interval(0.95, n - 1, loc=mean, scale=sqrt(var) / sqrt(n))[0], 
-                                                                  stats.t.interval(0.95, n - 1, loc=mean, scale=sqrt(var) / sqrt(n))[1],
-                                                                  sqrt((n - 1)*var/chisquare[1]), 
-                                                                   sqrt((n - 1)*var/chisquare[0]))
+    #m,b =  polyfit(x, y, 1)
+    #print "slope: %s, intercept: %s" % (m, b)
+    print "$%d$ & $%d$ & $%d$ & $%.1f$ & $%.1f$ & $%.1f-%.1f$ & $%.1f-%.1f$ \\\\" % (iteration[0], iteration[1], 
+                                                                                       iteration[2], mean, sqrt(var), t_test[0], t_test[1], sqrt((n - 1)*var/chisquare[1]), sqrt((n - 1)*var/chisquare[0]))
+    #print "$%d$ & $%.1f$ & $%.1f$ & $%.1f-%.1f$ & $%.1f-%.1f$" % (iteration, mean, sqrt(var), stats.t.interval(0.95, n - 1, loc=mean, scale=sqrt(var) / sqrt(n))[0], 
+    #                                                              stats.t.interval(0.95, n - 1, loc=mean, scale=sqrt(var) / sqrt(n))[1],
+    #                                                              sqrt((n - 1)*var/chisquare[1]), 
+    #                                                               sqrt((n - 1)*var/chisquare[0]))
+    fileHandle.close();
+    return p1
+
+def plotSpecial(logFile, windowSize = 10000, marker = 's', iteration = 50):
+    fileHandle = open(logFile, 'r')
+    
+    x = []
+    y1 = []
+    y2 = []
+    y3 = []
+    wholeFile = fileHandle.read();
+    wholeFile = wholeFile.split("\n")[:-1]
+    firstLine = wholeFile[0]
+    globalMin = float(firstLine.strip().split()[0])
+    for line in wholeFile[1:]:
+        values = line.strip().split()
+        timestamp = (float(values[0]) - globalMin)/1000.0
+        x.append(timestamp)
+        sizes = [int(k) for k in values[2:19]]
+        y1.append(numpy.mean(sizes))
+        y2.append(int(values[19]))
+        y3.append(int(values[20]))
+
+
+    p1, = plt.plot(x,y1, marker = 's')
+    p2, = plt.plot(x,y2, marker = 'o')
+    p3, = plt.plot(x,y3, marker = 'D')
+    ax = plt.gca()
+    ax.set_ylim([-100, 80000])
+    ax.set_xlabel('Experiment time (s)')
+    ax.set_ylabel('Number of messages stored per queue')
+    ax.set_title('Number of messages stored per queue over time')
+    plt.legend([p1,p2,p3], ["Larry queues", "Tola-Maruja queue", "Rick-Carl queue"])
     fileHandle.close();
     return p1
 
 def main():
-    logList = '/home/dballesteros/dataWorkplace/Experiment_5/clients-senders-50-0.log'
-    p1 = plotAverageResponseTime(logList, windowSize = 30*1000, marker = 'o')
-    logList = '/home/dballesteros/dataWorkplace/Experiment_5/clients-senders-25-0.log'
-    #print "Read with 25 readers:"
-    p2 = plotAverageResponseTime(logList, windowSize = 30*1000, marker = 's', iteration = 25)
-    logList = '/home/dballesteros/dataWorkplace/Experiment_5/clients-senders-16-0.log'
-    #print "Read with 16 readers:"
-    p3 = plotAverageResponseTime(logList, windowSize = 30*1000, marker = '^', iteration = 16)
-    logList = '/home/dballesteros/dataWorkplace/Experiment_5/clients-senders-12-0.log'
-    #print "Read with 12 readers:"
-    p4 = plotAverageResponseTime(logList, windowSize = 30*1000, marker = 'D', iteration = 12)
-    logList = '/home/dballesteros/dataWorkplace/Experiment_5/clients-senders-10-0.log'
-    #print "Read with 10 readers:"
-    p5 = plotAverageResponseTime(logList, windowSize = 30*1000, marker = '*', iteration = 10)
-    plt.legend([p1,p2,p3,p4,p5], ["1:1", "1:2", "1:3", "1:4", "1:5" ])
+    
+    #for x in [1, 50]:
+    #    for y in [10, 20]:
+    #        for z in [1, 5]:
+    logList = '/home/diegob/dataWorkplace/Experiment_8/log-full-trace-2h-2/watchdog.log'
+    p1 = plotSpecial(logList, windowSize = 60*1000, marker = 'o', iteration = [1, 20,5])
+    #logList = '/home/diegob/dataWorkplace/Experiment_7/log-50-10-1/clients-reader-0.log'
+    #p2 = plotAverageResponseTime(logList, windowSize = 60*1000, marker = 's', iteration = [1, 20,5])
+                    #print "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s," % (x, y, z, m, x*y, x*z, x*m, y*z, y*m, z*m, x*y*z, x*y*m, x*z*m, y*z*m, x*y*z*m, 1)
+    #plt.plot()
+    #p1 = plotAverageResponseTime(logList, windowSize = 60*1000, marker = 'o')
+    #p1 = plotThroughput(logList, windowSize = 60*1000, marker = 'o')
+    #logList = '/home/diegob/dataWorkplace/Experiment_6/log-2000-1-125-125/clients-senders-0.log'
+    #p2 = plotAverageResponseTime(logList, windowSize = 60*1000, marker = 's', iteration= [2000, 1, 125, 125])
+#     p2 = plotThroughput(logList, windowSize = 60*1000, marker = 's')
+#     logList = '/home/diegob/dataWorkplace/Experiment_6/log-2000-10-125-60/clients-reader-0.log'
+#     #p2 = plotAverageResponseTime(logList, windowSize = 60*1000, marker = 's')
+#     p3 = plotThroughput(logList, windowSize = 60*1000, marker = 'D')
+#     logList = '/home/diegob/dataWorkplace/Experiment_6/log-2000-10-125-125/clients-reader-0.log'
+#     #p2 = plotAverageResponseTime(logList, windowSize = 60*1000, marker = 's')
+#     p4 = plotThroughput(logList, windowSize = 60*1000, marker = '*')
+    #logList = '/home/diegob/dataWorkplace/Experiment_6/log-2000-10-125-60/clients-reader-0.log'
+    #p3 = plotAverageResponseTime(logList, windowSize = 60*1000, marker = '*')
+    #logList = '/home/diegob/dataWorkplace/Experiment_6/log-2000-10-60-60/clients-reader-0.log'
+    #p4 = plotAverageResponseTime(logList, windowSize = 60*1000, marker = 'D')
+    #plt.legend([p1,p2], ["Read operation", "Send operation"])
     plt.show()
     
     #plotAverageResponseTime(logList, eventKeyword = 'READ', windowSize = 120).show()
--- FTaB/python/Parsers/Plotters/LinePlotter.py
+++ FTaB/python/Parsers/Plotters/LinePlotter.py
@@ -9,29 +9,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-x = [10, 12, 16, 25, 50]
-y = [39.1759412212, 38.2569129843, 39.6075519783,39.1187951817, 78.2106472431]
-error = [ 87.251101961, 74.4513752202,88.3687896286,89.8709845297, 169.655824034,  ]
+x = [1, 10]
+y = [687.2, 652.7]
+error = [ 1271.2, 259.5]
 error_low = []
 for idx, z in enumerate(error):
     if z < y[idx]:
         error_low.append(z)
     else:
         error_low.append(y[idx])
-marker = 'bo--'
+marker = 'bo-'
 
 
-fit = polyfit(x,y,1)
-fit_fn = poly1d(fit)
 p1, = plt.plot(x,y, marker)
-p2, = plt.plot(x, fit_fn(x), 'rs-')
 ax = plt.gca()
 ax.errorbar(x, y, yerr=np.vstack([error_low, error]))#, marker = marker)
-ax.set_ylim([-10, 400])
-#ax.set_xlim([0, 7400])
-ax.set_xlabel('Total number of readers')
-ax.set_ylabel('Average response time (ms)')
-ax.set_title('Average response time to read a message vs number of readers in the system')
 
-plt.legend([p2], ["RT(c) = %f*c + %f" % (fit[0], fit[1])])
+y = [650.5, 666.8]
+error = [1200.4, 257.5]
+error_low = []
+for idx, z in enumerate(error):
+    if z < y[idx]:
+        error_low.append(z)
+    else:
+        error_low.append(y[idx])
+marker= 'rs-'
+p2, = plt.plot(x,y, marker)
+ax.errorbar(x, y, yerr=np.vstack([error_low, error]))#, marker = marker)
+
+
+
+#ax.set_ylim([-10, 2200])
+ax.set_xlim([0, 15])
+ax.set_xlabel('Number of queues')
+ax.set_ylabel('Average response time (ms)')
+ax.set_title('Average response time to send/read a message vs number of target queues\n (250 reader and sender clients, 2000 mesage size)')
+plt.legend([p1,p2], ["Read", "Send"])
+
 plt.show()
