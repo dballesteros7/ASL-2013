d79bb1a85694b73fba49eb3345ced5f6f8513184
Diego Ballesteros Villamizar <diegob@student.ethz.ch>
2013-12-15 00:32:06 +0100

    I'm Diego and I'm an idiot!

--- FTaB/tools/milestone_2_dryad/modify_trace_server_ssmt.sh
+++ FTaB/tools/milestone_2_dryad/modify_trace_server_ssmt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sed -i 's/<WorkerThreads>.*<\/WorkerThreads>/<WorkerThreads>'"$1"'<\/WorkerThreads>/g' /home/user25/config/milestone_2/config-server.xml
+sed -i 's/<DatabaseConnections>.*<\/DatabaseConnections>/<DatabaseConnections>'"$1"'<\/DatabaseConnections>/g' /home/user25/config/milestone_2/config-server.xml
 USERS_PER_THREAD=$((2*$2/$1))
 sed -i 's/<ClientsPerWorker>.*<\/ClientsPerWorker>/<ClientsPerWorker>'"$USERS_PER_THREAD"'<\/ClientsPerWorker>/g' /home/user25/config/milestone_2/config-server.xml
